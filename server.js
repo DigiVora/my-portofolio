5d46d6447b0560bb91183d9720bbbb2d08d43e46
DigiVora Studio <digivoraofficial81@gmail.com>
2026-05-29 11:25:50 +0700

    Change server port from 2056 to 3000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 2056;
+const PORT = process.env.PORT || 3000;
 
 // Menyajikan file statis dari folder 'dist'
 app.use(express.static(path.join(__dirname, 'dist')));
